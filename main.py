72ae49a24e751a6ffe4bf9527cf9ceb70377f8ac
99shoguns <shamrockoshaughnessy@gmail.com>
2025-12-17 11:47:14 -0800

    Asteroids and spawning

--- main.py
+++ main.py
@@ -2,17 +2,33 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Game variables
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # delta time
+
+    # Group variables
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Add to Groups
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    # Objects
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     # Run core game loop
     while(True):
@@ -27,8 +43,11 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt) # Update all objects in updatable group
+        # Draw all objects in drawable group
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = (clock.tick(60)) / 1000 # Limit to 60 fps and capture milliseconds since last tick
