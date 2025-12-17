d6fce98ac6085adf327acec7027cc9e623c0e3d3
99shoguns <shamrockoshaughnessy@gmail.com>
2025-12-17 11:22:35 -0800

    Player movement logic

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     # Run core game loop
     while(True):
         log_state()
+        keys = pygame.key.get_pressed()
 
+        # Handle escape key pressed or window being closed and stop game
+        if keys[pygame.K_ESCAPE]:
+            return
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000 # Limit to 60 fps and capture milliseconds since last tick
 
 if __name__ == "__main__":
     main()
